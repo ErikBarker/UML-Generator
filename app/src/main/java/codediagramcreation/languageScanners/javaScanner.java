328c79898184cfccb9c95f14b15ae767f493ddb1
Erik Barker <112636699+ErikBarker@users.noreply.github.com>
2024-05-10 12:25:37 -0400

    Modified javascanner class

--- app/src/main/java/codediagramcreation/languageScanners/javaScanner.java
+++ app/src/main/java/codediagramcreation/languageScanners/javaScanner.java
@@ -4,35 +4,49 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+import codediagramcreation.GeneralLanguageInfo.GeneralLanguage;
+
 //@Supported
 public class javaScanner {
-    
-
+    private static Scanner fileReader;
+    private static String currentline;
+    private static String[] currentlinearray;
 
     public static void scanFile(File file) throws FileNotFoundException {
         System.out.println("reading file " + file.getName());
-        Scanner fileReader = new Scanner(file);
-        String currentline;
-        String[] currentlinearray;
-
-        String[] accessingDeclaration = new String[2];
+       
+        fileReader = new Scanner(file);
 
         while (fileReader.hasNext()) {
             currentline = fileReader.nextLine();
 
-            if (currentline.contains("Class") || currentline.contains("class")) {
-                currentlinearray = currentline.split(" ");
+            javaClass();
+        }
+    }
 
-                for (int i = 0; i < accessingDeclaration.length; i++) {
-                    if (currentlinearray[i].equals("public") || currentlinearray[i].equals("private") || currentlinearray[i].equals("protected")) {
-                        accessingDeclaration[0] = currentlinearray[i];
-                    } else if (currentlinearray[i].equals("static")) {
-                        accessingDeclaration[1] = currentlinearray[i];
-                    } else if (condition) {
-                        
+    private static void javaClass(){
+        
+        String[] accessingDeclaration = new String[2];
+        String name;
+        if (currentline.contains("Class") || currentline.contains("class")) {
+            currentlinearray = currentline.split(" ");
+
+            for (int i = 0; i < accessingDeclaration.length; i++) {
+                if (currentlinearray[i].equals("public") || currentlinearray[i].equals("private") || currentlinearray[i].equals("protected")) {
+                    accessingDeclaration[0] = currentlinearray[i];
+                } else if (currentlinearray[i].equals("static")) {
+                    accessingDeclaration[1] = currentlinearray[i];
+                } else {
+                    name = currentlinearray[i];
+
+                    if (name.contains("{")) {
+                        name = name.replace("{", "");
                     }
                 }
             }
+
+            GeneralLanguage.generateClass();
+
         }
     }
 }
